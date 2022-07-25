52801508e205ea149ff442ae8d61f4aba4b0199a
FahmiEfendy <fahmi.efendy@binus.ac.id>
2022-07-25 10:50:09 +0700

    Todo List App with React

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,15 +4,6 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 export default function ExpenseItem(props) {
-  // title: props.title
-  // setTitle: function untuk merubah title
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle("Updated!");
-    console.log(title);
-  };
-
   return (
     <li>
       <Card className="expense-item">
@@ -21,7 +12,6 @@ export default function ExpenseItem(props) {
           <h2>{props.title}</h2>
           <div className="expense-item__price">${props.amount}</div>
         </div>
-        <button onClick={clickHandler}>Change Title</button>
       </Card>
     </li>
   );
